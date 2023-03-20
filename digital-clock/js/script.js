0856253d1ed65eb57550c71d5300d39b10f6b1c6
Oybek <programmeroybek211@gmail.com>
2023-03-20 18:40:07 +0500

    first commit

---
+++ digital-clock/js/script.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const hourEl=document.querySelector('#hour'),
+    minutesEl=document.querySelector('#minutes'),
+    secoundsEl=document.querySelector('#secounds'),
+    ampmEl=document.querySelector('#ampm')
+
+function updateClock(){
+    let h=new Date().getHours()
+    let m=new Date().getMinutes()
+    let s=new Date().getSeconds()
+
+    let ampm= "AM"
+
+    if (h>12){
+        h=h-12 
+        ampm="PM"
+    }
+
+    if(h<10){
+        h=`0${h}`
+    }
+    if(m<10){
+       m=`0${m}`
+    }
+    if(s<10){
+        s=`0${s}`
+    }
+
+hourEl.innerText=h
+minutesEl.innerText=m
+secoundsEl.innerText=s
+ampmEl.innerText=ampm
+setTimeout(() => {
+    updateClock()
+}, 1000);
+
+
+}
+
+updateClock()
+
+
